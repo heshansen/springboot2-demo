75b19526f6f4a63bfaf34accd06615f25bc7fdf9
heshansen <903532517@qq.com>
2018-10-18 11:23:26 +0800

    放开静态资源访问权限/css和/image下所有资源可直接访问

--- src/main/java/com/yss/sofa/licensedemo/config/WebSecurityConfig.java
+++ src/main/java/com/yss/sofa/licensedemo/config/WebSecurityConfig.java
@@ -37,7 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .authorizeRequests()
-                .antMatchers("/hello").permitAll()
+                .antMatchers("/hello","/image/*","/css/*").permitAll()
 //                .antMatchers("/user/**").hasRole("ROLE_ADMIN")  //此处要把 ROLE_ 去除
                 .antMatchers("/user/*").hasAuthority("ADMIN")
                 .anyRequest().authenticated()
